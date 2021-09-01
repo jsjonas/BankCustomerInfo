eee2959e061b8fbe222e6118d21895c20d24fdc6
monalisa <yona@marketing66.com>
2021-09-01 03:12:22 +0300

    Initial Commit

---
+++ lodumi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lodumi
+//
+//  Created by Jonas S on 01/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
